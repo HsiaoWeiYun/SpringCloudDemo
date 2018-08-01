df94f862a388109a680796cd142c1634f1c9eba3
Phoenix <hsiaoweiyun@gmail.com>
2018-08-01 21:45:19 +0800

    support redis sentinel

--- CalService/src/main/java/com/hsiaoweiyun/config/RedisConfig.java
+++ CalService/src/main/java/com/hsiaoweiyun/config/RedisConfig.java
@@ -6,6 +6,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -28,13 +30,24 @@ public class RedisConfig {
         return jedisPoolConfig;
     }
 
-    @Bean
+    /*@Bean
     JedisConnectionFactory jedisConnectionFactory() {
         JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
         jedisConnectionFactory.setHostName(redisProperties.getHost());
         jedisConnectionFactory.setPort(redisProperties.getPort());
         jedisConnectionFactory.setPoolConfig(createJedisPoolConfig());
         return jedisConnectionFactory;
+    }*/
+
+
+    @Bean
+    public RedisConnectionFactory jedisConnectionFactory() {
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+                .master("redismaster")
+                .sentinel("192.168.99.100", 26379)
+                .sentinel("192.168.99.100", 26380)
+                .sentinel("192.168.99.100", 26381);
+        return new JedisConnectionFactory(sentinelConfig);
     }
 
     @Bean
--- parent/build.sh
+++ parent/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 docker-compose -f ../docker-compose.yml down
+docker-compose -f ../redis/docker-compose.yml down
 docker rmi eureka:1.0 calservice:1.0 consumerservice:1.0
+docker rmi redis-demo:latest redis-sentinel-demo:latest
+docker network rm spring-cloud-demo
+docker network create -d bridge spring-cloud-demo
 mvn -T4 clean package
+docker-compose -f ../redis/docker-compose.yml up -d
 docker-compose -f ../docker-compose.yml up -d
