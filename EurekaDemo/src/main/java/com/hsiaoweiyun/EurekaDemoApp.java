a1bcc8467baaf33da254e20a558dddfab889eb31
Phoenix <hsiaoweiyun@gmail.com>
2018-10-23 22:38:40 +0800

    add log

--- EurekaDemo/src/main/java/com/hsiaoweiyun/EurekaDemoApp.java
+++ EurekaDemo/src/main/java/com/hsiaoweiyun/EurekaDemoApp.java
@@ -1,13 +1,17 @@
 package com.hsiaoweiyun;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@Slf4j
 public class EurekaDemoApp {
     public static void main(String[] args) {
+        log.debug("##################################################################################sdsdsd");
         SpringApplication.run(EurekaDemoApp.class, args);
     }
 }
