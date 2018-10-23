e996b45c103a90cd92ecaa31b5aa090ed99a6fd5
Phoenix <hsiaoweiyun@gmail.com>
2018-10-23 23:06:57 +0800

    add springloaded

--- EurekaDemo/src/main/java/com/hsiaoweiyun/EurekaDemoApp.java
+++ EurekaDemo/src/main/java/com/hsiaoweiyun/EurekaDemoApp.java
@@ -5,13 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaServer
+@ComponentScan(basePackages = "com.hsiaoweiyun.*")
 @Slf4j
 public class EurekaDemoApp {
     public static void main(String[] args) {
-        log.debug("##################################################################################sdsdsd");
         SpringApplication.run(EurekaDemoApp.class, args);
     }
 }
